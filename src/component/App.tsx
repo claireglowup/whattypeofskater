76489a5e28ddd908d8220fd4424dafe63e2c0aa3
claire <claireglowup@gmail.com>
2024-10-29 02:50:16 +0700

    initial

---
+++ src/component/App.tsx
@@ -0,0 +1,11 @@
+import "../style/App.css";
+
+function App() {
+  return (
+    <div className="container-header">
+      <h1 className="header-title">what type of skater do you want to be ?</h1>
+    </div>
+  );
+}
+
+export default App;
