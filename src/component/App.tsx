00f437adf2a3864d3e80b95ac714ff58fef242d0
claire <claireglowup@gmail.com>
2024-10-30 02:34:39 +0700

    add teamof

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,9 +2,50 @@ import "../style/App.css";
 
 function App() {
   return (
-    <div className="container-header">
-      <h1 className="header-title">what type of skater do you want to be ?</h1>
-    </div>
+    <>
+      <div className="container-header">
+        <h1 className="header-title">
+          what type of skater do you want to be ?
+        </h1>
+      </div>
+      <div className="content">
+        <h2>team of</h2>
+        <div className="card-container">
+          <div className="card">
+            <img
+              src="https://i.pinimg.com/564x/49/2a/27/492a27d4ca9caa65492543a12f1a8e18.jpg"
+              alt=""
+              width={200}
+            />
+            <p>supreme</p>
+          </div>
+          <div className="card">
+            <img
+              src="https://i.pinimg.com/564x/49/2a/27/492a27d4ca9caa65492543a12f1a8e18.jpg"
+              alt=""
+              width={200}
+            />
+            <p>supreme</p>
+          </div>
+          <div className="card">
+            <img
+              src="https://i.pinimg.com/564x/49/2a/27/492a27d4ca9caa65492543a12f1a8e18.jpg"
+              alt=""
+              width={200}
+            />
+            <p>supreme</p>
+          </div>
+          <div className="card">
+            <img
+              src="https://i.pinimg.com/564x/49/2a/27/492a27d4ca9caa65492543a12f1a8e18.jpg"
+              alt=""
+              width={200}
+            />
+            <p>supreme</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
