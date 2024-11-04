f70e380f0d885733268b5fd85ad45f76c8059749
claire <claireglowup@gmail.com>
2024-11-05 00:52:02 +0700

    reboisasi

--- src/component/TeamPage.tsx
+++ src/component/TeamPage.tsx
@@ -5,7 +5,7 @@ export const TeamPage = () => {
     <div className="container-team">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/1200px-Supreme_Logo.svg.png" alt="supreme" width={500} />
       <h1>Supreme</h1>
-      <p>some word here soon</p>
+      <p>some word here soon!</p>
     </div>
   );
 };
