3c670dbf9abb553d969b80c05be9139c8d28fba1
claire <claireglowup@gmail.com>
2024-11-04 09:27:36 +0700

    add heading team

--- src/component/TeamPage.tsx
+++ src/component/TeamPage.tsx
@@ -1,12 +1,11 @@
+/** @format */
+
 export const TeamPage = () => {
   return (
     <div className="container-team">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/1200px-Supreme_Logo.svg.png"
-        alt="supreme"
-        width={500}
-      />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/1200px-Supreme_Logo.svg.png" alt="supreme" width={500} />
       <h1>Supreme</h1>
+      <p>some word here soon</p>
     </div>
   );
 };
